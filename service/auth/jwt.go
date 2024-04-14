43b2eb1d6f2dba3de30c5243d1fcb7cb68722d99
Chiril Dones <doneskirill3@gmail.com>
2024-04-14 06:31:53 +0300

    add jwt (login, register)

---
+++ service/auth/jwt.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"github.com/doneskkk/order-api/config"
+	"github.com/golang-jwt/jwt/v5"
+	"strconv"
+	"time"
+)
+
+func CreateJWT(secret []byte, userID int) (string, error) {
+	expiration := time.Second * time.Duration(config.Envs.JWTExpiration)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userID":    strconv.Itoa(userID),
+		"expiredAt": time.Now().Add(expiration).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
--- service/user/routes.go
+++ service/user/routes.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"fmt"
+	"github.com/doneskkk/order-api/config"
 	"github.com/doneskkk/order-api/service/auth"
 	"github.com/doneskkk/order-api/types"
 	"github.com/doneskkk/order-api/utils"
@@ -24,6 +25,41 @@ func (h *Handler) RegisterRoutes(router chi.Router) {
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+
+	//get JSON payload
+	var newUser types.LoginUser
+	if err := utils.ParseJSON(r, &newUser); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(newUser); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
+
+	u, err := h.repo.GetUserByEmail(newUser.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		return
+	}
+
+	if !auth.ComparePasswords(u.Password, []byte(newUser.Password)) {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		return
+	}
+
+	secret := []byte(config.Envs.JWTSecret)
+	token, err := auth.CreateJWT(secret, u.ID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, map[string]string{"token": token})
+
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
@@ -43,7 +79,7 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 	//check if the user exists
 	_, err := h.repo.GetUserByEmail(newUser.Email)
-	if err == nil {
+	if err != nil {
 		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", newUser.Email))
 		return
 	}
