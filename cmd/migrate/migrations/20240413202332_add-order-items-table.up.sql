23d49fe37227b4e5ca5882ee7d008c69f5720aec
Chiril Dones <doneskirill3@gmail.com>
2024-04-14 05:54:35 +0300

    add migrations

---
+++ cmd/migrate/migrations/20240413202332_add-order-items-table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS order_items (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    orderId INT UNSIGNED NOT NULL,
+    productId INT UNSIGNED NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (orderId) REFERENCES orders(id),
+    FOREIGN KEY (productId) REFERENCES products(id)
+)
